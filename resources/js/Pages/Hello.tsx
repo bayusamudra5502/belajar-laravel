1cacbf2b92b502449349357d9068bed7142d2a92
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2023-06-10 17:00:26 +0700

    add react

---
+++ resources/js/Pages/Hello.tsx
@@ -0,0 +1,12 @@
+import {Head} from "@inertiajs/react";
+
+export default function Hello() {
+    return (
+        <>
+            <Head>
+                <title>Hello World</title>
+            </Head>
+            <h1>Hello World</h1>
+        </>
+    )
+}
